8221ce24a772a5924a399774100f59f3bc6b721b
NivesR <nives.rombini@gmail.com>
2024-09-04 09:55:46 +0200

    make more things optionals in the abstract base model

--- defs/abstract_class.py
+++ defs/abstract_class.py
@@ -17,13 +17,11 @@ class Abstract(BaseModel):
     """Information extracted from an academic abstract."""
     abstract_number: str = Field(description="The unique 4-digit identifier for the abstract, excluding any non-numeric characters")
     title: str = Field(description="The complete title of the abstract, preserving capitalization and any special characters")
-    authors: List[Author] = Field(description="A comprehensive list of all authors mentioned, including their full names and institutional affiliations if provided")
+    authors: Optional[List[Author]] = Field(description="A comprehensive list of all authors mentioned, including their full names and institutional affiliations if provided")
     disease: Optional[List[str]] = Field(description="A list of specific cancer indications or types mentioned in the abstract")
     gene: Optional[List[str]] = Field(description="A list of genes mentioned in the abstract.")
 #    interaction: Optional[List[GeneDisease]] = Field(description="A list of gene-disease interactions mentioned in the abstract")
     gene_target: Optional[str] = Field(description="The primary gene target that impacts the disease and can be modulated to change the disease")
-    organism: Optional[List[str]] = Field(description="The types of organism used in the study. Must be 'cell line', 'PDX', 'animal', 'human', or 'n/a' if not specified.")
-    trial_stage: Optional[str] = Field(description="The stage of the trial or study. Must be 'preclinical', 'Phase I', 'Phase II', 'Phase III', 'post-approval','n/a'")
-    compound_name: Optional[str] = Field(description="The name of the compound or drug mentioned in the abstract. Examples: 'GS-P-328', 'IOMX-0675', or 'Pembrolizumab'")
-    abstract_category: str = Field(description="Assign the abstract to one of the following categories: 'Diagnostics', 'Gene-Disease Associations', or 'Other'. Choose the most appropriate category based on the main focus of the abstract.")
+    organism: Optional[List[str]] = Field(description="The types of organism used in the study. Must be 'cell line', 'PDX', 'animal', 'human', or 'n/a' if not applicable or not specified.")
+    compound_name: Optional[str] = Field(description="The name of the compound or drug mentioned in the abstract. E.g. 'GS-P-328', 'IOMX-0675', or 'Pembrolizumab', or 'n/a' if not applicable or not specified.")
 
