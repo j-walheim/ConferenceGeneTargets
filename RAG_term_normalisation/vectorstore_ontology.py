2ef971bda22ac472a0b9c43a9aa350faae72a7f6
NivesR <nives.rombini@gmail.com>
2024-09-06 10:38:18 +0200

    simplify code for vectorstore

--- RAG_term_normalisation/vectorstore_ontology.py
+++ RAG_term_normalisation/vectorstore_ontology.py
@@ -3,38 +3,24 @@ import numpy as np
 import faiss
 from sentence_transformers import SentenceTransformer
 from tqdm import tqdm
+import logging
 
 class VectorStore:
     def __init__(self, model_name='all-MiniLM-L6-v2', vectorstore_dir="data/RAG_LLM/vectorstore"):
         self.model = SentenceTransformer(model_name)
         self.vectorstore_dir = vectorstore_dir
         self.dimension = self.model.get_sentence_embedding_dimension()
-        self.gene_index = None
-        self.disease_index = None
-        self.gene_lookup = None
-        self.disease_lookup = None
+        self.indexes = {}
+        self.lookups = {}
 
-    def prepare_gene_lookup(self, gene_synonyms):
-        self.lookup = [
-            (synonym, row['Symbol'])
-            for _, row in gene_synonyms.iterrows()
-            for synonym in (row['Synonyms'].split(';') if row['Synonyms'] != '-' else [row['Symbol']])
+    def prepare_lookup(self, data, name, synonym_col, official_col):
+        self.lookups[name] = [
+            (synonym, row[official_col])
+            for _, row in data.iterrows()
+            for synonym in row[synonym_col].split(';') if row[synonym_col] != '-'
         ]
 
-    def prepare_disease_lookup(self, disease_synonyms):
-        self.lookup = [
-            (synonym, row['disease'])
-            for _, row in disease_synonyms.iterrows()
-            for synonym in row['synonym'].split(';')
-        ]
-
-    def add_to_index(self, index, lookup):
-        for synonym, _ in tqdm(lookup, desc="Adding to index"):
-            embedding = self.model.encode([synonym])
-            index.add(np.array(embedding))
-
-
-    def create_or_load_index(self, name, lookup):
+    def create_or_load_index(self, name):
         index = faiss.IndexFlatL2(self.dimension)
         vectorstore_path = os.path.join(self.vectorstore_dir, f"vectorstore_{name}.index")
         if os.path.exists(vectorstore_path):
@@ -42,23 +28,22 @@ class VectorStore:
             index = faiss.read_index(vectorstore_path)
         else:
             print(f"Creating new {name} vectorstore at {vectorstore_path}")
-            self.add_to_index(index, lookup)
+            for synonym, _ in tqdm(self.lookups[name], desc=f"Adding to {name} index"):
+                index.add(np.array(self.model.encode([synonym])))
             faiss.write_index(index, vectorstore_path)
-        return index
+        self.indexes[name] = index
 
     def prepare_vectorstores(self):
         os.makedirs(self.vectorstore_dir, exist_ok=True)
-        self.gene_index = self.create_or_load_index("genes", self.gene_lookup)
-        self.disease_index = self.create_or_load_index("diseases", self.disease_lookup)
+        for name in self.lookups:
+            self.create_or_load_index(name)
 
-    def retrieve(self, index, query, k=5):
-        query_vector = self.model.encode([query])
-        D, I = index.search(query_vector, k)
-        res = [self.lookup[i] for i in I[0]]
+    def retrieve(self, name, query):
+        D, I = self.indexes[name].search(np.array(self.model.encode([query])), k=5)
+        logging.info(f"Query: {query}, Search results: D={D}, I={I}")
         
-        return [f"Primary: '{primary}'; Synonym: '{synonym}'" for synonym, primary in res]
+        results = [self.lookups[name][i] for i in I[0] if i < len(self.lookups[name])]
+        return ", ".join(results) if results else ""
 
-    def rag(self, query):
-        disease_context = self.retrieve(self.disease_index, query, k=2)
-        gene_context = self.retrieve(self.gene_index, query)
-        return gene_context, disease_context
+    def rag(self, name, query):
+        return self.retrieve(name, query)
